662e6bbda9f3827ad5e158391d85dee903c64c94
Vít Kutný <vitkutny@outlook.com>
2014-11-17 01:27:45 +0100

    dev

--- src/Alias/Extension.php
+++ src/Alias/Extension.php
@@ -3,8 +3,52 @@
 namespace WebEdit\Alias;
 
 use Nette;
+use Fuel;
+use WebEdit;
 
 final class Extension extends Nette\DI\CompilerExtension
 {
 
+	private $defaults = [
+		'prepend' => TRUE,
+		'class' => [],
+		'namespace' => [],
+		'pattern' => []
+	];
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->getConfig($this->defaults);
+		$manager = $builder->addDefinition($this->prefix('manager'))
+			->setClass(Fuel\Alias\Manager::class);
+		foreach ($config['class'] as $original => $alias) {
+			$manager->addSetup('alias', [
+				$original,
+				$alias
+			]);
+		}
+		foreach ($config['namespace'] as $original => $alias) {
+			$manager->addSetup('aliasNamespace', [
+				$original,
+				$alias
+			]);
+		}
+		foreach ($config['pattern'] as $original => $alias) {
+			$manager->addSetup('aliasPattern', [
+				$original,
+				$alias
+			]);
+		}
+	}
+
+	public function afterCompile(Nette\PhpGenerator\ClassType $class)
+	{
+		$config = $this->getConfig($this->defaults);
+		$initialize = $class->methods['initialize'];
+		$initialize->addBody('$this->getByType(?)->register(?);', [
+			Fuel\Alias\Manager::class,
+			$config['prepend'] ? 'prepend' : 'append'
+		]);
+	}
 }
